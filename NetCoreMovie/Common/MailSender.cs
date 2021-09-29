2a4611013d215179418e8df6b4c676324eeb370b
Kadir kenan <kadirkenan.kk@gmail.com>
2021-09-29 08:42:10 +0300

    Login Sayfası ve Mail gönderme işlemi.

---
+++ NetCoreMovie/Common/MailSender.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class MailSender
+    {
+        public static void SendEmail(string email, string subject, string message)
+        {
+            //MailMessage
+            MailMessage sender = new MailMessage();
+            sender.From = new MailAddress("ba.yzl3148@gmail.com", "YZL3148");
+            sender.To.Add(email);
+            sender.Subject = subject;
+            sender.Body = message;
+
+            //SmtpClient
+            SmtpClient smtp = new SmtpClient();
+            smtp.Credentials = new NetworkCredential("ba.yzl3148@gmail.com", "Yzl3148!!--");
+            smtp.Port = 587;//ssl
+            smtp.Host = "smtp.gmail.com";
+            smtp.EnableSsl = true;
+
+            smtp.Send(sender);
+        }
+    }
+}
---
+++ NetCoreMovie/WebUI/Controllers/HomeController.cs
@@ -0,0 +1,118 @@
+﻿using DataAccess.Entity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebUI.Models;
+using WebUI.Models.ViewModels;
+
+namespace WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+        public HomeController(UserManager<AppUser> _usermanager, SignInManager<AppUser> _signInManager)
+        {
+            userManager = _usermanager;
+            signInManager = _signInManager;
+        }     
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        ////////////////////////////////////////////////////////////////////////////
+
+      
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (ModelState.IsValid)
+            {
+
+                AppUser newUser = new AppUser()
+                {
+                    UserName = registerVM.UserName,
+                    Email = registerVM.Email
+                };
+
+                var result = await userManager.CreateAsync(newUser, registerVM.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(error.Code, error.Description);
+                    }
+                }
+
+
+            }
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await userManager.FindByEmailAsync(loginVM.Email);
+                if (user != null)
+                {
+                    var result = await signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return View("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("failedLogin", "Şifre Hatalı!");
+                        return View();
+                    }
+
+                }
+                else
+                {
+                    ModelState.AddModelError("notFound", "Böyle Bir Kullanıcı Bulunamadı!");
+                    return View();
+                }
+            }
+            else
+                return View();
+        }
+
+
+
+    }
+}
